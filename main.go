9e6e8ad2523367c14ca9d3b12e6daac778ddb1a9
elliot <masterharoldsaxon4@gmail.com>
2019-12-01 12:37:49 -0500

    Beta

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
+	"strings"
 
 	"github.com/BurntSushi/toml"
 )
@@ -152,7 +156,7 @@ func GetXAdToken(XClient, UserAgent, XAuthToken string) string {
 
 type TomlConfig struct {
 	XAuthToken string `toml:"XAuthToken"`
-	XAdToken   string `toml:"organization"`
+	XAdToken   string `toml:"XAdToken"`
 }
 
 func Init() (string, string, string, string) {
@@ -160,7 +164,7 @@ func Init() (string, string, string, string) {
 	if fileExists("config.toml") {
 		fmt.Println("config.toml exists")
 		var config TomlConfig
-		if _, err := toml.DecodeFile("example.toml", &config); err != nil {
+		if _, err := toml.DecodeFile("config.toml", &config); err != nil {
 			fmt.Println(err)
 
 		}
@@ -207,7 +211,7 @@ func (h Hash) SearchBook(target string) Request {
 	req.Header.Add("X-CLIENT-IDENTIFIER", h.XClient)
 	req.Header.Add("User-Agent", h.UserAgent)
 	req.Header.Add("X-AUTH-TOKEN", h.XAuthToken)
-	req.Header.Add("X-AUTH-TOKEN", h.XAdToken)
+	req.Header.Add("X-AD-TOKEN", h.XAdToken)
 	resp, err := client.Do(req)
 	check(err)
 	defer resp.Body.Close()
@@ -218,7 +222,174 @@ func (h Hash) SearchBook(target string) Request {
 	check(err)
 	return result
 }
+func Select() int {
 
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Println("---------------------")
+	fmt.Println("Select book, please!")
+	fmt.Print("-> ")
+	text, _ := reader.ReadString('\n')
+	// convert CRLF to LF
+	text = strings.Replace(text, "\n", "", -1)
+	z, _ := strconv.Atoi(string(text))
+	//	fmt.Println(z)
+
+	return z
+}
+func Parse(id int) map[int]string {
+	var masser = map[int]string{}
+	path := strconv.Itoa(id) + "/fileList.m3u8"
+	file, err := os.Open(path)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	data := make([]byte, 64)
+	mass := ""
+	for {
+		n, err := file.Read(data)
+		if err == io.EOF { // если конец файла
+			break // выходим из цикла
+		}
+		mass = mass + string(data[:n])
+	}
+	s := strings.Split(mass, "segment")
+	z := len(s)
+	i := 1
+	for i < z {
+		masser[i-1] = "/segment" + strings.Split(s[i], ".aac")[0] + ".aac"
+		i++
+	}
+	i = i - 1
+	//fmt.Println(masser)
+	//fmt.Printf("%d block\n", i)
+	//fmt.Println("---------------------")
+
+	//	fmt.Println("---------------------")
+	//	fmt.Println("<-  Download")
+	//	Download(masser, id)
+	return masser
+}
+func (h Hash) Stream(id int) {
+	//	/stream/hls/preview/13613/fileList.m3u8
+	path := strconv.Itoa(id) + "/fileList.m3u8"
+	url := "https://n01.cd.ru.patephone.com/stream/hls/ad/" + path
+	fmt.Println(url)
+	client := &http.Client{}
+	req, err := http.NewRequest(
+		"GET", url, nil,
+	)
+	req.Header.Add("X-CLIENT-IDENTIFIER", h.XClient)
+	req.Header.Add("User-Agent", h.UserAgent)
+	req.Header.Add("X-AUTH-TOKEN", h.XAuthToken)
+	req.Header.Add("X-AD-TOKEN", h.XAdToken)
+	//	req.Header.Add("Accept-Encoding", "gzip")
+	//	req.Header.Add("Connection", "Keep-Alive")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error", err)
+		return
+	}
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("err %s", err)
+		os.Exit(1)
+	}
+	fmt.Println("---------------------")
+	fmt.Println("<-  Content")
+	cont := string(contents)
+	fmt.Println(string(contents))
+	fmt.Println("---------------------")
+	out, err := os.Create(path)
+	check(err)
+	defer out.Close()
+	_, err = out.WriteString(cont)
+	check(err)
+	// Write the body to file
+	//_, err = io.Copy(out, []byte(cont))
+	//check(err)
+
+}
+func (h Hash) GetInfo(id int) GetInfoStruct {
+	url := "https://api.patephone.com/client-api/books/" + strconv.Itoa(id)
+	fmt.Println(url)
+	client := &http.Client{}
+	req, err := http.NewRequest(
+		"GET", url, nil,
+	)
+	req.Header.Add("X-CLIENT-IDENTIFIER", h.XClient)
+	req.Header.Add("User-Agent", h.UserAgent)
+	req.Header.Add("X-AUTH-TOKEN", h.XAuthToken)
+	req.Header.Add("X-AD-TOKEN", h.XAdToken)
+	resp, err := client.Do(req)
+	check(err)
+	defer resp.Body.Close()
+	contents, err := ioutil.ReadAll(resp.Body)
+	oscheck(err)
+	//fmt.Printf("content: %s\n", string(contents))
+	var infoRes GetInfoStruct
+	err = json.Unmarshal(contents, &infoRes)
+	check(err)
+
+	//fmt.Println("id=", infoRes.Book.ID, " | ", infoRes.Book.Title, ". ", infoRes.Book.Authors[1].FirstName, " ", infoRes.Book.Authors[1].LastName, "\n", infoRes.Book.Description, infoRes.Book.Duration, infoRes.Book.FileSize, infoRes.Book.PreviewStreamURL)
+	//Stream(infoRes.Book.ID) // MUST BE OPTIMIZED SEND Id
+	return infoRes
+}
+func ChlkDir(id int) {
+	path := strconv.Itoa(id)
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		os.Mkdir(path, 0777)
+	}
+}
+func (h Hash) DownloadFile(path string) error {
+
+	// Get the data
+	url := "https://n01.cd.ru.patephone.com/stream/hls/ad/" + path
+	fmt.Println(url)
+	client := &http.Client{}
+	req, err := http.NewRequest(
+		"GET", url, nil,
+	)
+	req.Header.Add("X-CLIENT-IDENTIFIER", h.XClient)
+	req.Header.Add("User-Agent", h.UserAgent)
+	req.Header.Add("X-AUTH-TOKEN", h.XAuthToken)
+	req.Header.Add("X-AD-TOKEN", h.XAdToken)
+	req.Header.Add("Accept-Encoding", "identity")
+	req.Header.Add("Connection", "Keep-Alive")
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("error", err)
+		return err
+	}
+	defer resp.Body.Close()
+	fmt.Println(resp)
+	// Create the file
+
+	out, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+
+	// Write the body to file
+	_, err = io.Copy(out, resp.Body)
+	return err
+}
+func (h Hash) Download(dict map[int]string, id int) {
+	fmt.Println(dict)
+	n := len(dict)
+	path := ""
+	for i := 0; i < n; i++ {
+		path = strconv.Itoa(id) + dict[i]
+		if err := h.DownloadFile(path); err != nil {
+			panic(err)
+		}
+	}
+}
 func main() {
 	XClient, XAuthToken, XAdToken, UserAgent := Init()
 	api := &Hash{
@@ -228,6 +399,27 @@ func main() {
 		UserAgent,
 	}
 	api.Hello()
-	bookSearch := api.SearchBook("Автостопом")
-	fmt.Println(bookSearch)
+	bookSearch := api.SearchBook("time")
+	n := bookSearch.Paging.Count
+	if n > 0 {
+		for i := 0; i < n; i++ {
+			fmt.Println(i, "\b.) ", bookSearch.Book[i].ID, " : ", bookSearch.Book[i].Title) //9411
+		}
+	} else {
+		fmt.Println("book not found")
+	}
+	idNum := Select()
+	fmt.Println("you selected:", idNum, "\b.)", bookSearch.Book[idNum].Title)
+	fmt.Println("---------------------")
+	info := api.GetInfo(bookSearch.Book[idNum].ID)
+	fmt.Printf(">>%d \n>>%s \n>>%s \n>>%s \n>>%s \n>>%d \n>>%d \n>>%s \n", info.Book.ID, info.Book.Title, info.Book.Authors[1].FirstName, info.Book.Authors[1].LastName, info.Book.Description, info.Book.Duration, info.Book.FileSize, info.Book.PreviewStreamURL)
+	fmt.Println("<-  ChlkDir")
+	ChlkDir(info.Book.ID)
+	fmt.Println("---------------------")
+	api.Stream(info.Book.ID)
+	parseList := Parse(info.Book.ID)
+	fmt.Println("parseList:\n", parseList)
+	fmt.Println("---------------------")
+	api.Download(parseList, info.Book.ID)
+
 }
